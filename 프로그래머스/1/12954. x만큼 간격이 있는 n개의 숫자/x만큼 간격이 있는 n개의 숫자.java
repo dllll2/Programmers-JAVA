978d0e499122551b7a506e341f2f56280073cc41
slatio <105922173+dllll2@users.noreply.github.com>
2024-11-18 09:48:39 +0900

    Update x만큼 간격이 있는 n개의 숫자.java

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -5,6 +5,7 @@ class Solution {
         int result = 0;
         // 일반항 : xn = x + (n-1)x
         // 첫째항 = x, 공차 = x
+        // 일반항 = n*x
         for (int i = 1; i <= n; i++) {
             answer[i - 1] = (long) i * x; // 등차수열 계산 및 저장
         }             
